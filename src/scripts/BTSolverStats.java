d8ac02f274f15d99f0013de8e2a6bdef01ceb7e8
Mr_Little_Kitty <nuclearcat1337@gmail.com>
2017-03-09 14:57:02 -0800

    Finished up testing setup

--- src/scripts/BTSolverStats.java
+++ src/scripts/BTSolverStats.java
@@ -3,6 +3,7 @@ package scripts;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -75,6 +76,7 @@ public class BTSolverStats {
 		ArrayList<String> output = new ArrayList<>();
 
 		int i = 1;
+		outer:
 		for(ValueSelectionHeuristic valueSelect : ValueSelectionHeuristic.values())
 		{
 			for(VariableSelectionHeuristic variableSelect : VariableSelectionHeuristic.values())
@@ -83,6 +85,7 @@ public class BTSolverStats {
 				{
 					for(BTSolver.HeuristicCheck heuristicCheck : BTSolver.HeuristicCheck.values())
 					{
+						System.out.println("Starting next combination.");
 						easyStats.clear();
 						mediumStats.clear();
 						hardStats.clear();
@@ -96,8 +99,10 @@ public class BTSolverStats {
 
 						output.add(statsToString(consistencyCheck,valueSelect,variableSelect,heuristicCheck,easyStats,mediumStats,hardStats));
 
-						System.out.print("Completed combination "+i+"/72.");
+						System.out.println("Completed combination "+i+"/72.");
 						i++;
+						if(i == 11)
+							break outer;
 					}
 				}
 			}
@@ -153,7 +158,72 @@ public class BTSolverStats {
 										List<runStats> medium,
 										List<runStats> hard)
 	{
+		int easyPercent = 0;
+		for(runStats s : easy)
+			if(s.isSolution())
+				easyPercent++;
+		easyPercent = (int)(((double)easyPercent/(double)easy.size())*100);
+
+		int medPercent = 0;
+		for(runStats s : medium)
+			if(s.isSolution())
+				medPercent++;
+		medPercent = (int)(((double)medPercent/(double)medium.size())*100);
+
+		int hardPercent = 0;
+		for(runStats s : hard)
+			if(s.isSolution())
+				hardPercent++;
+		hardPercent = (int)(((double)hardPercent/(double)hard.size())*100);
+
+		List<runStats> all = new ArrayList<>(easy);
+		all.addAll(medium);
+		all.addAll(hard);
+
+		long totalRunTime = 0;
+		long totalAssignments = 0;
+		long totalBackTracks = 0;
+		int totalSuccessful = 0;
+
+		for(runStats rs : all)
+		{
+			if(rs.isSolution())
+			{
+				totalRunTime += rs.getRuntime();
+				totalAssignments += rs.getNumAssignments();
+				totalBackTracks += rs.getNumBacktracks();
+				totalSuccessful++;
+			}
+		}
+
 		StringBuilder builder = new StringBuilder();
-		return null;
+		builder
+				.append(String.format("%16s",consistencyCheck.name()))//16
+				.append(" | ")
+				.append(String.format("%23s",valueSelectionHeuristic.name()))//16
+				.append(" | ")
+				.append(String.format("%22s",variableSelectionHeuristic.name()))//16
+				.append(" | ")
+				.append(String.format("%13s", heuristicCheck.name()))//16
+				.append(" | ");
+
+		builder
+				.append(String.format("%15s",(int)(totalRunTime/totalSuccessful)))
+				.append(" | ")
+				.append(String.format("%10s",(int)(totalAssignments/totalSuccessful)))
+				.append(" | ")
+				.append(String.format("%10s",(int)(totalBackTracks/totalSuccessful)))
+				.append(" | ");
+
+		builder
+				.append(String.format("%4s%%",(int)(easyPercent)))
+				.append(" | ")
+				.append(String.format("%4s%%",(int)(medPercent)))
+				.append(" | ")
+				.append(String.format("%4s%%",(int)(hardPercent)))
+				.append(" | ");
+
+
+		return builder.toString();
 	}
 }
